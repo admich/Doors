c4ce54087608775dacac7725d2081ce7b7f2b196
admich <andrea.demichele@gmail.com>
2021-01-23 23:06:25 +0100

    clean package

--- package.lisp
+++ package.lisp
@@ -18,68 +18,16 @@
 (defpackage #:clim-doors
   (:use :clim :clim-lisp :clim-backend :clim-xcommon :clim-clx)
   (:import-from :alexandria
-                #:when-let*)
+                #:when-let*
+                #:when-let)
   (:import-from :climi
-                #:+alt-key+
-                ;;
-                #:port-text-style-mappings
-                #:port-lookup-mirror
-                #:port-register-mirror
-                #:port-event-process
                 #:port-grafts
-                #:%%sheet-native-transformation
-                #:%%set-sheet-native-transformation
-                #:device-transformation
-                ;;
-                #:clamp
-                #:get-environment-variable
-                #:pixmap-sheet
-                #:port-lookup-sheet
-                #:port-unregister-mirror
-                #:port-pointer-sheet
-                #:map-repeated-sequence
-                #:pixmap-mirror
-                #:do-sequence
-                #:with-double-buffering
-                #:with-transformed-position
-                #:with-transformed-positions
-                #:with-medium-options
-                ;;
-                #:pixmap
-                #:top-level-sheet-mixin
-                #:unmanaged-sheet-mixin
                 #:top-level-sheet-pane
-                #:unmanaged-top-level-sheet-pane
-                #:menu-frame
-                ;;
-                #:frame-managers        ;used as slot
-                #:top-level-sheet       ;used as slot
-                #:medium-device-region
-                #:draw-image
-                #:height                ;this seems bogus
-                #:width                 ;dito
-                #:coordinate=
-                #:get-transformation
-                ;;
-                #:medium-miter-limit
-                ;; classes
-                #:mirrored-pixmap
                 #:window-destroy-event
-                #:pointer-grab-enter-event
-                #:pointer-grab-leave-event
-                #:pointer-ungrab-leave-event
-                #:pointer-ungrab-enter-event
-                #:device-font-text-style
-                ;; utils
-                #:dolines
+                #:frame-managers
                 #:maybe-funcall
-                #:when-let
-                #:if-let)
-  (:import-from #:climi
-                #:event-listen-or-wait
                 #:%sheet-mirror-region
-                #:%sheet-mirror-transformation
-                #:standard-port)  
+                #:%sheet-mirror-transformation)
   (:export
    #:*wm-application*
    #:active-frame
